bca506c7ef8bdbdea2f0aa05faa7e9f42290bf6c
der_frühling <works-on-my-machine@der-fruhling.mozmail.com>
2024-04-22 10:46:28 -0700

    #14 Implement fixed standing position

--- common/src/main/java/net/derfruhling/minecraft/create/trainperspective/CreateTrainPerspectiveMod.java
+++ common/src/main/java/net/derfruhling/minecraft/create/trainperspective/CreateTrainPerspectiveMod.java
@@ -8,20 +8,24 @@ import net.minecraft.client.player.AbstractClientPlayer;
 import net.minecraft.client.player.LocalPlayer;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.player.Player;
+import org.apache.commons.lang3.mutable.MutableInt;
 import org.slf4j.Logger;
 
 import java.util.HashMap;
+import java.util.Map;
 import java.util.UUID;
 
 // The value here should match an entry in the META-INF/mods.toml file
 public class CreateTrainPerspectiveMod {
     // Define mod id in a common place for everything to reference
     public static final String MODID = "create_train_perspective";
+    public static CreateTrainPerspectiveMod INSTANCE;
     // Directly reference a slf4j logger
     private static final Logger LOGGER = LogUtils.getLogger();
 
     public CreateTrainPerspectiveMod() {
-        TickEvent.PLAYER_POST.register(this::onTickPlayer);
+        TickEvent.PLAYER_POST.register(this::tickPlayer);
+        INSTANCE = this;
     }
 
     private static class RotationState {
@@ -59,7 +63,26 @@ public class CreateTrainPerspectiveMod {
         }
     }
 
-    public void onTickPlayer(final Player player) {
+    public void tickStandingPlayers(final CarriageContraptionEntity contraption) {
+        for(Map.Entry<Entity, MutableInt> entry : contraption.collidingEntities.entrySet()) {
+            var entity = entry.getKey();
+            var ticks = entry.getValue();
+            if(entity instanceof Player player) {
+                if(ticks.getValue() >= 2) {
+                    var persp = (PlayerPerspectiveBehavior) Minecraft.getInstance().getEntityRenderDispatcher().getRenderer(player);
+                    states.remove(player.getUUID());
+                    persp.disable();
+                } else {
+                    var persp = (PlayerPerspectiveBehavior) Minecraft.getInstance().getEntityRenderDispatcher().getRenderer(player);
+                    var state = new RotationState(contraption);
+                    states.put(player.getUUID(), state);
+                    persp.enable(state.entity.pitch, state.entity.yaw);
+                }
+            }
+        }
+    }
+
+    public void tickPlayer(final Player player) {
         if (!(player instanceof AbstractClientPlayer)) return;
         if(states.containsKey(player.getUUID())) {
             var state = states.get(player.getUUID());
---
+++ common/src/main/java/net/derfruhling/minecraft/create/trainperspective/mixin/ContraptionColliderMixin.java
@@ -0,0 +1,23 @@
+package net.derfruhling.minecraft.create.trainperspective.mixin;
+
+import com.simibubi.create.content.contraptions.AbstractContraptionEntity;
+import com.simibubi.create.content.contraptions.ContraptionCollider;
+import com.simibubi.create.content.trains.entity.CarriageContraptionEntity;
+import net.derfruhling.minecraft.create.trainperspective.CreateTrainPerspectiveMod;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(ContraptionCollider.class)
+public class ContraptionColliderMixin {
+    @Inject(method = "collideEntities", at = @At("HEAD"), remap = false)
+    private static void saveClientPlayerFromClipping(
+            AbstractContraptionEntity contraptionEntity,
+            CallbackInfo ci
+    ) {
+        if(contraptionEntity instanceof CarriageContraptionEntity carriage) {
+            CreateTrainPerspectiveMod.INSTANCE.tickStandingPlayers(carriage);
+        }
+    }
+}
