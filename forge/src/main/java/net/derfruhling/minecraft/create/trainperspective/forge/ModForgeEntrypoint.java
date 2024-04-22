9b8245259a874fce33d29c7e8582c5894221f8a0
der_frühling <works-on-my-machine@der-fruhling.mozmail.com>
2024-04-22 16:08:52 -0700

    Fix: Stop doing anything on a dedicated server

--- forge/src/main/java/net/derfruhling/minecraft/create/trainperspective/forge/ModForgeEntrypoint.java
+++ forge/src/main/java/net/derfruhling/minecraft/create/trainperspective/forge/ModForgeEntrypoint.java
@@ -2,9 +2,12 @@ package net.derfruhling.minecraft.create.trainperspective.forge;
 
 import dev.architectury.platform.forge.EventBuses;
 import net.derfruhling.minecraft.create.trainperspective.CreateTrainPerspectiveMod;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.api.distmarker.OnlyIn;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.entity.EntityMountEvent;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import org.jetbrains.annotations.NotNull;
 
@@ -13,11 +16,16 @@ public class ModForgeEntrypoint {
     public CreateTrainPerspectiveMod mod = new CreateTrainPerspectiveMod();
 
     public ModForgeEntrypoint() {
+        MinecraftForge.EVENT_BUS.addListener(this::onClientSetupEvent);
+    }
+
+    private void onClientSetupEvent(FMLClientSetupEvent event) {
         EventBuses.registerModEventBus(CreateTrainPerspectiveMod.MODID, FMLJavaModLoadingContext.get().getModEventBus());
 
         MinecraftForge.EVENT_BUS.addListener(this::onEntityMountEvent);
     }
 
+    @OnlyIn(Dist.CLIENT)
     private void onEntityMountEvent(@NotNull EntityMountEvent event) {
         mod.onEntityMount(
                 event.isMounting(),
