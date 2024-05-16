c395fa90f10c9be653e0424be1de3ea204127222
der_frühling <works-on-my-machine@der-fruhling.mozmail.com>
2024-05-16 01:50:07 -0700

    #35 partially implement better block raycasts

--- common/src/main/java/net/derfruhling/minecraft/create/trainperspective/mixin/EntityMixin.java
+++ common/src/main/java/net/derfruhling/minecraft/create/trainperspective/mixin/EntityMixin.java
@@ -1,14 +1,18 @@
 package net.derfruhling.minecraft.create.trainperspective.mixin;
 
+import com.llamalad7.mixinextras.sugar.Local;
 import net.derfruhling.minecraft.create.trainperspective.CreateTrainPerspectiveMod;
+import net.derfruhling.minecraft.create.trainperspective.Perspective;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
+import net.minecraft.client.Minecraft;
+import net.minecraft.util.Mth;
 import net.minecraft.world.entity.Entity;
+import net.minecraft.world.level.Level;
 import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.*;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
@@ -16,6 +20,7 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 @Environment(EnvType.CLIENT)
 public abstract class EntityMixin {
     @Shadow @Nullable private Entity vehicle;
+    @Shadow private Level level;
 
     @Inject(method = "startRiding(Lnet/minecraft/world/entity/Entity;Z)Z", at = @At(value = "RETURN", ordinal = 4))
     public void onStartRiding(Entity entity, boolean bl, CallbackInfoReturnable<Boolean> cir) {
@@ -27,5 +32,15 @@ public abstract class EntityMixin {
         if(vehicle != null) {
             CreateTrainPerspectiveMod.INSTANCE.onEntityMount(false, (Entity)(Object)this, vehicle);
         }
+    }
+
+    @SuppressWarnings("UnreachableCode")
+    @ModifyVariable(method = "calculateViewVector", at = @At(value = "LOAD"), index = 1, argsOnly = true)
+    public float adjustViewVector(float original, @Local(argsOnly = true, index = 2) float yRot) {
+        if (this.level.isClientSide) {
+            if (Minecraft.getInstance().getEntityRenderDispatcher().getRenderer((Entity)(Object)this) instanceof Perspective persp && persp.isEnabled()) {
+                return original - persp.getLean() * Mth.sin((persp.getYaw() - yRot) * Mth.DEG_TO_RAD);
+            } else return original;
+        } else return original;
     }
 }
