c620861ab3f86d43ab1ad3f7bdac15b568449866
der_frühling <works-on-my-machine@der-fruhling.mozmail.com>
2024-06-09 17:47:50 -0700

    remove this comment

--- common/src/main/java/net/derfruhling/minecraft/create/trainperspective/mixin/LocalPlayerMixin.java
+++ common/src/main/java/net/derfruhling/minecraft/create/trainperspective/mixin/LocalPlayerMixin.java
@@ -10,7 +10,6 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 
-// If anything else tries to @Overwrite getViewYRot, use their changes.
 @Mixin(value = LocalPlayer.class)
 public class LocalPlayerMixin {
     @Shadow @Final protected Minecraft minecraft;
